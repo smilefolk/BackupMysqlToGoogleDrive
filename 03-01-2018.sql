488ad49a184f0fdd5f164394dfc0c2e858e28811
Pipat Thammakosol <smilefolkk@gmail.com>
2018-01-11 11:11:08 +0700

    remove mysql

--- 03-01-2018.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
-INSERT INTO `user` (`id`,`first_name`,`last_name`) VALUES (1,'pipat','thammakosol');
--- 05-01-2018.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(50) NOT NULL,
-  `last_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
-INSERT INTO `user` (`id`,`first_name`,`last_name`) VALUES (1,'pipat','thammakosol');
-INSERT INTO `user` (`id`,`first_name`,`last_name`) VALUES (2,'test','test');
