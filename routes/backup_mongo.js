fb3f580fa6c5dd3d61ea667842c26049fbb3121c
Pipat Thammakosol <smilefolkk@gmail.com>
2018-01-15 15:58:46 +0700

    update

--- routes/backup_mongo.js
+++ routes/backup_mongo.js
@@ -1,7 +1,7 @@
 var cmd     = require('node-command-line'),
     Promise = require('bluebird');
 function runSingleCommandWithoutWait() {
-    cmd.run('mongodump --out /jenkins_folk/BackupMysqlToGoogleDrive/mongodb --username mmt --password "maxworldchampion2016"');
+    cmd.run('mongodump --username mmt --password "maxworldchampion2016" --out /jenkins_folk/BackupMysqlToGoogleDrive/mongodb');
     console.log('Executed your command :)');
   }
 runSingleCommandWithoutWait()
