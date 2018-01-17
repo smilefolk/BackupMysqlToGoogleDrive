cb20d58be2c2a8e5f48f10531d52f0317d5c4a54
Pipat Thammakosol <smilefolkk@gmail.com>
2018-01-17 11:02:37 +0700

    Add Cron Backup MongoDB

--- routes/backup_mongo.js
+++ routes/backup_mongo.js
@@ -1,7 +1,10 @@
 var cmd     = require('node-command-line'),
     Promise = require('bluebird');
+var cron    = require('node-cron');
 function runSingleCommandWithoutWait() {
     cmd.run('mongodump --host beta.maxmuaythai.com --port 27017 --username mmt --password "maxworldchampion2016" --db max_api_phase2 --out /jenkins_folk/BackupMysqlToGoogleDrive/mongodb');
     console.log('Executed your command :)');
 }
-runSingleCommandWithoutWait()
+cron.schedule('* * * 1 * *',function() {
+    runSingleCommandWithoutWait()
+})
